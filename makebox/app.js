d36882494681d36fcd6eff7174dba57a2c2fa86f
임동균 <70128434+Dong-Es@users.noreply.github.com>
2022-09-21 23:20:53 +0900

    Update and rename asdasd/123 to makebox/app.js

---
+++ makebox/app.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Box from './box';
+
+function App() {
+  return (
+    <main>
+    <Box name="Soulmaster" num="1"/>
+    <Box name="Nightwalker" num="2"/>
+    <Box name="Windbreaker" num="3"/>
+    <Box name="FlameWizzard" num="4"/>
+    </main>
+  );
+}
+
+export default App;
